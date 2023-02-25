15b47416515aaefdb3f2a1651948595dddb32647
Pranshul <pranshul.hatwar@gmail.com>
2023-02-25 10:52:17 +0530

    Player movement added

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ window.addEventListener('load',function(){
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
-            this.collisionRadius = 50
+            this.collisionRadius = 30
+            this.speedX = 0;
+            this.speedY = 0;
+            this.dx = 0; // distance b/w mouse and player horizontally
+            this.dy = 0; // distance b/w mouse and player vertically
+            this.speedModifier = 5; // to modify the speed
         }
         // draw method is used to draw and animate player
         // context specify which canvas we use to draw
@@ -30,7 +35,25 @@ window.addEventListener('load',function(){
             context.fill();
             context.restore();
             context.stroke(); 
-
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY); // defines the starting coordinates
+            context.lineTo(this.game.mouse.x, this.game.mouse.y); // defines the ending coordinates
+            context.stroke(); // to actually draw the line
+        }
+        update() {
+            // to move the player in the direction of the mouse
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY;
+            const distance = Math.hypot(this.dy, this.dx);
+            if(distance > this.speedModifier) {
+                this.speedX = this.dx/distance || 0;
+                this.speedY = this.dy/distance || 0;
+            }else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
         }
     }
 
@@ -50,32 +73,35 @@ window.addEventListener('load',function(){
             canvas.addEventListener('mousedown', (e) => {
                 this.mouse.x = e.offsetX;
                 this.mouse.y = e.offsetY;
-                this.mouse.y = true;
-                console.log(this.mouse);
+                this.mouse.pressed = true;
             });
             canvas.addEventListener('mouseup', (e) => {
                 this.mouse.x = e.offsetX;
                 this.mouse.y = e.offsetY;
-                this.mouse.y = false;
-                console.log(this.mouse);
+                this.mouse.pressed = false;
             });
             canvas.addEventListener('mousemove', (e) => {
-                this.mouse.x = e.offsetX;
-                this.mouse.y = e.offsetY;
-                this.mouse.y = false;
-                console.log(this.mouse);
+                if(this.mouse.pressed) {
+                    this.mouse.x = e.offsetX;
+                    this.mouse.y = e.offsetY;
+                    this.mouse.pressed = false;
+                }
             });
         }
+
         render(context) {
             this.player.draw(context);
+            this.player.update();
         }
     }
 
     const game = new Game(canvas);
-    game.render(ctx);
-    console.log(game);
+
 
     function animate () {
-
+        ctx.clearRect(0,0, canvas.width, canvas.height); // clear the previous area
+        game.render(ctx);
+        requestAnimationFrame(animate); // endless animation loop
     }
+    animate();
 });
