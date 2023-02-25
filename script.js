8e5e77274983242f78f99002ec168a050ad19189
Pranshul <pranshul.hatwar@gmail.com>
2023-02-25 14:18:54 +0530

    sprite generation added

--- script.js
+++ script.js
@@ -65,8 +65,16 @@ window.addEventListener('load',function(){
             this.collisionX = Math.random() * this.game.width;
             this.collisionY = Math.random() * this.game.height;
             this.collisionRadius = 40;
+            this.image = document.getElementById('obstacles')
+            this.spriteWidth = 250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX - (this.width * 0.5);
+            this.spriteY = this.collisionY - (this.height * 0.5) - 70;
         }
         draw(context) {
+            context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
             context.beginPath();
             context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
             context.save();
@@ -83,7 +91,7 @@ window.addEventListener('load',function(){
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
-            this.noOfObstacles = 5;
+            this.noOfObstacles = 1;
             this.obstacles = []
             this.mouse = {
                 x: this.width * 0.5,
